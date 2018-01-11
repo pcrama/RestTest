c3e3966ef7d799aa9b6def55905c0598e86f7102
Philippe Crama <pcfeb0009@gmx.com>
2018-01-11 18:20:44 +0100

    Initial commit of property testing experiment for RestSharp

---
+++ src/Tester/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open RestSharp
+open RestSharp.Deserializers
+
+open FsCheck
+
+// let urlIsNotLost (uri:string) = RestClient(uri).BaseUrl = new Uri(uri)
+
+let accept2types((t1, t2):string * string) =
+    if t1 = null || t2 = null || t1 = "" || t2 = "" || t1 = t2
+    then
+       true
+    else
+        let mutable add2ThenRemoveSecond = RestClient();
+        let mutable addOnlyFirst = RestClient();
+        add2ThenRemoveSecond.ClearHandlers();
+        addOnlyFirst.ClearHandlers();
+        add2ThenRemoveSecond.AddHandler(t1, JsonDeserializer());
+        addOnlyFirst.AddHandler(t1, JsonDeserializer());
+        add2ThenRemoveSecond.AddHandler(t2, XmlDeserializer());
+        add2ThenRemoveSecond.RemoveHandler(t2);
+        add2ThenRemoveSecond.DefaultParameters = addOnlyFirst.DefaultParameters
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!";
+    Check.One({ Config.Quick with MaxTest = 10; QuietOnSuccess=false }, accept2types);
+    0 // return an integer exit code
